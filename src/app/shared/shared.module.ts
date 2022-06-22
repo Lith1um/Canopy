9fbd26befd9ad8aaa4bdcd262054b815dbbae6af
Alex Rayner <alexrayner94@gmail.com>
2022-06-22 14:20:03 +0100

    adding standard modules & configure tailwind

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MaterialModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MaterialModule
+  ],
+  declarations: [],
+  providers: [],
+})
+export class SharedModule {}
